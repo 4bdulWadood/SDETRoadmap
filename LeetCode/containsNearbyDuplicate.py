16d53ceb090fb29e3c534596b227bff09237f51c
Syed Wadood <57127141+4bdulWadood@users.noreply.github.com>
2025-05-09 10:25:15 -0400

    Create containsNearbyDuplicate.py

---
+++ LeetCode/containsNearbyDuplicate.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        
+        seen = set()
+
+        for i in range(len(nums)):
+            if nums[i] in seen:
+               return True;
+            
+            seen.add(nums[i])
+
+            if(len(seen) > k):
+                seen.remove(nums[i-k])
+        
+        return False;
+
+#Time and Space Complexity :
+#Time : O(n)
+#Space : O(k)
