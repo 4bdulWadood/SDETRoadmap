21ee635d90027b884f06d2776d70f6fe9fcf0463
Syed Wadood <57127141+4bdulWadood@users.noreply.github.com>
2025-05-11 17:05:47 -0400

    Create minimum-depth-of-binary-tree.py

---
+++ LeetCode/minimum-depth-of-binary-tree.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        if not root.left:
+            return 1 + self.minDepth(root.right)
+        if not root.right:
+            return 1 + self.minDepth(root.left)
+
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+
+        # Recursive implement so Space Complexity O(Log N), Time Complexity O(n), where n is the node count. 
