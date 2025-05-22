d92eed475ba5dbb49ebe19c8ab619900a34bd807
Syed Wadood <57127141+4bdulWadood@users.noreply.github.com>
2025-05-21 20:39:53 -0400

    Create replaceFirstN.py

---
+++ LeetCode/replaceFirstN.py
@@ -0,0 +1,5 @@
+result = text.replace("Python", "JavaScript", 2)
+# The count parameter in .replace() follows a strict left-to-right order.
+# 1. Scans the string frmo left to right.
+# 2. Replaces the first 2 occurrences of "Python" that it finds.
+# 3. Leaves any further occurences unchanged. 
