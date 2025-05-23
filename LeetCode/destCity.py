0457260f2b68c63c66be56cf77dd79bc0c00c231
Syed Wadood <57127141+4bdulWadood@users.noreply.github.com>
2025-05-23 11:52:51 -0400

    Create destCity.py

---
+++ LeetCode/destCity.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def destCity(self, paths):
+        """
+        :type paths: List[List[str]]
+        :rtype: str
+
+        2 Conditions need to be met to return the correct destination city.
+        1. City must belong to index 1.
+        2. City must only exist once.
+
+        Time Complexity : O(n)
+        Space Complexity : O(n)
+
+        Beats 100.00% of submissions :) 
+        0 ms Runtime.
+    
+
+
+        """
+        
+        city_dict = {}
+        for path in paths:
+            if path[1] not in city_dict:
+                city_dict[path[1]] = 1
+            else:
+                city_dict[path[1]] += 1
+
+        # Mark all starting cities with -1
+        for path in paths:
+            city_dict[path[0]] = -1
+        
+        for city in city_dict:
+            if city_dict[city] == 1:
+               return city
+
